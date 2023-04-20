c5608788d6e1647746a5c1d0634304f6f7018bd2
iammark <mrdamarkomm@gmail.com>
2023-04-20 18:54:54 +0200

    update 2.1

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,54 @@ import React from "react";
 import dev from "../assets/developer.jpg";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { Link } from "react-scroll";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 function Home() {
+
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/marko-mrda-a2011321b/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+         <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/markk300",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+         <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:mrdamarkomm@gmail.com",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+           <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "./Mrda Marko cv111.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
   return (
     <div name="home" className=" h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center 
@@ -19,7 +65,7 @@ function Home() {
           </p>
           <div>
             <Link to="portfolio" smooth duration={500}  className="group text-white w-fit px-6 py-3 flex items-center 
-            rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            rounded-md bg-gradient-to-r mb-5 from-cyan-500 to-blue-500 cursor-pointer">
                 Portfolio
                 <span className="group-hover:rotate-90 duration-300">
                 <RiArrowRightSLine size={25} className="ml-1" />
@@ -30,6 +76,28 @@ function Home() {
         <div>
             <img src={dev} alt="developer img" className="rounded-2xl mx-auto w-2/3 
             md:w-full "  />
+        </div>
+        <div>
+        <ul className="md:hidden flex justify-between gap-3 pt-10">
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className="flex justify-between items-center  h-14
+       px-4 bg-gray-500 rounded-xl hover:scale-110 duration-200  "
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full
+           text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
         </div>
       </div>
     </div>
